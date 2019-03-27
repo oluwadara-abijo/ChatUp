98b7075eb03b994a1b9b6472d5b3dd76509653af
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-03-27 11:52:52 +0100

    Connect to the MQTT broker

--- app/src/main/java/com/example/chatup/MainActivity.java
+++ app/src/main/java/com/example/chatup/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.chatup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize Mqtt client
+        String clientId = MqttClient.generateClientId();
+        MqttAndroidClient mqttClient = new MqttAndroidClient(getApplicationContext(),
+                "tcp://broker.hivemq.com:1883", clientId);
+
+        //Connect to client
+        try {
+            IMqttToken token = mqttClient.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d(LOG_TAG, "onSuccess");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.d(LOG_TAG, "onFailure");
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
